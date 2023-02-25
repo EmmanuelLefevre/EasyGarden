35ab9fd5e7e27a69e1e15197b04cc6820a01cff3
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-02-25 21:04:51 +0100

    pattern import

--- pwa/src/app/public/public.module.ts
+++ pwa/src/app/public/public.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
