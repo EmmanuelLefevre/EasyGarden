cb47c9950c811dc8df076b2278596d74fcdbf58f
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-02-25 13:34:02 +0100

    Footer SEO + tooltip

--- pwa/src/app/components/footer/footer.component.ts
+++ pwa/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+// Add ViewEncapsulation for resolve problems with loading custom scss .mat-tooltip in style.scss
 
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
@@ -6,7 +7,8 @@ import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class FooterComponent implements OnInit {
