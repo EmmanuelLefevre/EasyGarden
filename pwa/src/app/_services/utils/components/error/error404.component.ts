f60668371555add269d92106a40c66214bdd2782
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-02-27 11:05:56 +0100

    reset

--- pwa/src/app/_services/utils/components/error/error404.component.ts
+++ pwa/src/app/_services/utils/components/error/error404.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 
+
 @Component({
   selector: 'app-error',
   templateUrl: './error404.component.html',
