7d2ee1fea68a870a81c4cc43ba20c7b00176cda7
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-03-29 17:11:29 +0200

    input focus directive

---
+++ pwa/src/app/_directives/inputFocus/inputFocus.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, OnInit, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[inputFocus]'
+})
+
+
+export class InputFocusDirective implements OnInit {
+
+  constructor(private el: ElementRef) {}
+
+  /**
+   * Focus input on component init
+   */
+  ngOnInit() {
+    this.el.nativeElement.focus();
+  } 
+
+}
