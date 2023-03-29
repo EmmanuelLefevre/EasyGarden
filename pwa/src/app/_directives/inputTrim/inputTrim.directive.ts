65d69766d4e5ec3ba61edd60d913b4dcc5a31021
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-03-29 16:59:48 +0200

    indentation + comment

--- pwa/src/app/_directives/inputTrim/inputTrim.directive.ts
+++ pwa/src/app/_directives/inputTrim/inputTrim.directive.ts
@@ -5,15 +5,16 @@ import { NgControl } from '@angular/forms';
   selector: '[inputTrim]'
 })
 
+
 export class InputTrimDirective {
 
   @Output() ngModelChange = new EventEmitter();
 
   constructor(private el: ElementRef,
-    private control: NgControl) {}
+              private control: NgControl) {}
 
   /**
-   * Trim the input value on focus out of input component
+   * Trim the input value on focus out
    */
   @HostListener('focusout') onFocusOut() {
     (this.el.nativeElement as HTMLInputElement).value = (this.el.nativeElement as HTMLInputElement).value.trim();
