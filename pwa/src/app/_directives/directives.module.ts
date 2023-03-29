d4a293aa9025ae3afd2705d9fbb6ef3a1004422f
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-03-29 17:11:29 +0200

    input focus directive

--- pwa/src/app/_directives/directives.module.ts
+++ pwa/src/app/_directives/directives.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+import { InputFocusDirective } from './inputFocus/inputFocus.directive';
 import { InputTrimDirective } from './inputTrim/inputTrim.directive';
 
 
 @NgModule({
-  declarations: [InputTrimDirective],
-  exports: [InputTrimDirective]
+  declarations: [
+    InputTrimDirective,
+    InputFocusDirective],
+  exports: [
+    InputTrimDirective,
+    InputFocusDirective
+  ]
 })
 
 export class DirectivesModule { }
