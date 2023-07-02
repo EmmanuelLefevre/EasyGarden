893091a7d433c953cd0039c1993c669a3fed650d
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-07-02 16:10:20 +0200

    rename and add new directive

--- pwa/src/app/_directives/searchInputReset/search-input-reset.directive.ts
+++ pwa/src/app/_directives/searchInputReset/search-input-reset.directive.ts
@@ -2,12 +2,12 @@ import { Directive, HostListener, Input, ElementRef } from '@angular/core';
 
 
 @Directive({
-  selector: '[inputClear]'
+  selector: '[searchInputReset]'
 })
 
-export class InputClearDirective {
+export class SearchInputResetDirective {
 
-  @Input('inputSearch') searchInput: any;
+  @Input('searchInput') searchInput: any;
 
   constructor(private elementRef: ElementRef) {}
 
@@ -18,10 +18,15 @@ export class InputClearDirective {
     const isSelect = this.elementRef.nativeElement.contains(target.closest('select.filter-select-garden'));
     const isTh = this.elementRef.nativeElement.contains(target.closest('th.order'));
     const isTd = this.elementRef.nativeElement.contains(target.closest('td.power'));
+    const isPagination = target.closest('.pagination li');
 
-    if (!isSelect && !isTh && !isTd) {
+    console.log('isPagination:', isPagination);
+    console.log('Pagination Element:', target.closest('div.pagination > pagination-controls > pagination-template > nav > ul.ngx-pagination > li'));
+    
+    if (!isSelect && !isTh && !isTd && (!isPagination && this.searchInput.name == '')) {
       this.searchInput.name = '';
     }
+
   }
 
 }
