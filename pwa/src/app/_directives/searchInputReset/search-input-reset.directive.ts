fb0b4d5dd4b588d949b8cbb223eccbcbb91d6ec4
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-07-03 14:34:06 +0200

    add forgotten control ngForm.invalid on the select

--- pwa/src/app/_directives/searchInputReset/search-input-reset.directive.ts
+++ pwa/src/app/_directives/searchInputReset/search-input-reset.directive.ts
@@ -12,14 +12,19 @@ export class SearchInputResetDirective {
   constructor(private elementRef: ElementRef) {}
 
   /**
-   * Reset search input to an empty string when click somewhere in an equipment component unless if it's an order, filter or update status method
+   * Reset search input to an empty string when click somewhere in the component unless if it's an order/filter/updateStatus method or a pagination click
    */
   @HostListener('document:click', ['$event.target']) onClick(target: HTMLElement) {
     const isSelect = this.elementRef.nativeElement.contains(target.closest('select.filter-select-garden'));
     const isTh = this.elementRef.nativeElement.contains(target.closest('th.order'));
     const isTd = this.elementRef.nativeElement.contains(target.closest('td.power'));
-    
-    if (!isSelect && !isTh && !isTd) {
+    // const isPagination = target.closest('div.pagination > pagination-controls > pagination-template > nav > ul.ngx-pagination > li');
+    const isPagination = target.closest('.pagination') !== null;
+
+    // if (!isSelect && !isTh && !isTd) {
+    //   this.searchInput.name = '';
+    // }
+    if (!isSelect && !isTh && !isTd && (!isPagination && this.searchInput.name == '')) {
       this.searchInput.name = '';
     }
 
