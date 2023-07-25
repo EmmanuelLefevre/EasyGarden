f84397f0a6b24e8a6f8b6206bf860ac2e3b0dec4
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-07-25 19:21:03 +0200

    seperate email logic in a service

--- api/src/Controller/AccountCreationController.php
+++ api/src/Controller/AccountCreationController.php
@@ -4,37 +4,34 @@ namespace App\Controller;
 
 use App\DataPersister\UserDataPersister;
 use App\Entity\User;
+use App\Service\AccountCreationEmailService;
+use App\Utility\TokenGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AccountCreationController extends AbstractController
 {
     private $userDataPersister;
     private $userPasswordHasher;
+    private $emailService;
 
-    public function __construct(UserDataPersister $userDataPersister, UserPasswordHasherInterface $userPasswordHasher)
+    public function __construct(UserDataPersister $userDataPersister, 
+                                UserPasswordHasherInterface $userPasswordHasher, 
+                                AccountCreationEmailService $emailService)
     {
         $this->userDataPersister = $userDataPersister;
         $this->userPasswordHasher = $userPasswordHasher;
-    }
-
-    private function generateActivationToken(): string
-    {
-        // Generate a random activation token
-        return bin2hex(random_bytes(32));
+        $this->emailService = $emailService;
     }
 
     /**
      * @Route("/account_creation", name="account_creation", methods={"POST"})
      */
-    public function accountCreation(Request $request, MailerInterface $mailer, UrlGeneratorInterface $urlGenerator, UserDataPersister $userDataPersister): JsonResponse
+    public function accountCreation(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
@@ -57,63 +54,23 @@ class AccountCreationController extends AbstractController
         $user->setPassword($hashedPassword);
         $user->eraseCredentials();
 
-        // Generate an activation token for the user
-        $activationToken = $this->generateActivationToken();
+        // Generate and set the activation token for the user using the TokenGenerator class
+        $activationToken = TokenGenerator::generateToken();
         $user->setActivationToken($activationToken);
 
-        // Send the account creation confirmation email with activation link
-        $activationLink = $urlGenerator->generate('account_activation', ['token' => $activationToken], UrlGeneratorInterface::ABSOLUTE_URL);
-
-        // Interpolate the first name and last name into the subject
-        $subject = sprintf('Veuillez activer votre compte %s %s.', $user->getFirstName(), $user->getLastName());
-
-        // Activation button
-        $activationButton = sprintf(
-            '<a href="%s" style="display: inline-block; 
-                                background-color: #95cb11; 
-                                color: #ffffff; 
-                                border: 2px solid #C4F253; 
-                                padding: 6px 10px; 
-                                text-decoration: none; 
-                                border-radius: 4px; 
-                                transition: background-color 0.3s, color 0.3s;
-                                height: 20px;
-                                line-height: 20px;"
-                                >Activer le compte
-            </a>', 
-            $activationLink);
-
-        // Generate email body as plain text
-        $message = sprintf(
-            "Bonjour %s %s, merci pour votre inscription à Easygarden !\n\nVeuillez cliquer sur le lien suivant pour activer votre compte svp => %s\n\n",
-            $user->getFirstName(),
-            $user->getLastName(),
-            $activationLink
-        );
-        $message .= "Ce lien restera valide pour une durée de 48H à compter de la réception de ce message, une fois ce délai expiré il vous sera nécessaire de vous inscrire à nouveau.";
-
-        // Generate email body as HTML
-        $htmlMessage = sprintf(
-            'Bonjour %s %s, merci pour votre inscription à <a href="https://easygarden.com" style="color: #95cb11; text-decoration: none;">Easygarden</a> !<br><br>',
-            $user->getFirstName(),
-            $user->getLastName()
-        );
-        $htmlMessage .= 'Veuillez cliquer sur le lien suivant pour activer votre compte svp => ' . '<br><br>' . $activationButton . '<br><br>';
-        $htmlMessage .= 'Ce lien restera valide pour une durée de 48H à compter de la réception de ce message, une fois ce délai expiré il vous sera nécessaire de vous inscrire à nouveau.';
-
-        // Generate email
-        $email = (new Email())
-            ->from('easygarden.noreply@gmail.com')
-            ->to($user->getEmail())
-            ->subject($subject)
-            ->text($message)
-            ->html($htmlMessage);
-
-        $mailer->send($email);
+        // Send the account creation confirmation email with token activation link
+        // $this->emailService->sendActivationEmail($user, $user->getEmail(), $activationToken);
+        try {
+            $this->emailService->sendActivationEmail($user, $user->getEmail(), $activationToken);
+        } catch (\Exception $e) {
+            return new JsonResponse(['message' => 'Failed to send email'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        
+        // Persist User
+        $this->userDataPersister->persist($user);
 
         // Return a success response
         return new JsonResponse(['message' => 'Email was sent successfully'], Response::HTTP_OK);
-    
     }
 
 
@@ -132,9 +89,7 @@ class AccountCreationController extends AbstractController
         // Activate the user's account
         $user->setIsVerified(true);
 
-        // Persist the user using the UserDataPersister only after account confirmation
-        $userDataPersister->persist($user);
-
         return new JsonResponse(['status' => 201], Response::HTTP_CREATED);
     }
+
 }
