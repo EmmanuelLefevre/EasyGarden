5473b78b2378243967c91e0b188c5c75fb90fcf8
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-07-27 00:42:29 +0200

    fix logic in onSubmit()

--- pwa/src/app/public/components/login/login.component.ts
+++ pwa/src/app/public/components/login/login.component.ts
@@ -69,34 +69,34 @@ export class LoginComponent {
     }
     
     const typedLoginForm: ICredentials = this.loginForm.value;
-    const email = this.loginForm.value.email;
 
-    this.authService.isAccountVerified(email).subscribe(
-      (isVerified) => {
-        if (isVerified) {
-          this.authService.logIn(typedLoginForm).subscribe(
-            data => {
-              this.tokenService.saveToken(data.token);
+    this.authService.logIn(typedLoginForm).subscribe(
+      data => {
+        this.tokenService.saveToken(data.token);
+        const email = this.loginForm.value.email;
+        this.authService.isAccountVerified(email).subscribe(
+          (isVerified) => {
+            if (isVerified) {
               this.router.navigate(['easygarden']);
               this.snackbarService.showNotification(`Bonjour ${this.decodedTokenService.firstNameDecoded()} 
-                                              ${this.decodedTokenService.lastNameDecoded()}.`, 'logIn-logOut');
-            },
-            error => {
-              if (error.status === 401) {
-                this.errorMessage = "Identifiants incorrects!";
-                this.invalidCredentials = true;
-              }
-              else {
-                this.errorMessage = "Une erreur s'est produite lors de la connexion!";
-              }
+                                          ${this.decodedTokenService.lastNameDecoded()}.`, 'logIn-logOut');
+            } else {
+              this.snackbarService.showNotification(`Votre compte n'a pas encore été activé grâce au lien dans l'email qui vous a été envoyé!`, 'account-not-verified');
             }
-          );
-        } else {
-          this.snackbarService.showNotification(`Votre compte n'a pas encore été activé grâce au lien dans l'email qui vous a été envoyé!`, 'account-not-verified');
-        }
+          },
+          _error => {
+            this.snackbarService.showNotification(`Une erreur s'est produite lors de la vérification du compte!`, 'account-not-verified');
+          }
+        );
       },
-      _error => {
-        this.snackbarService.showNotification(`Une erreur s'est produite lors de la vérification du compte!`, 'account-not-verified');
+      error => {
+        if (error.status === 401) {
+          this.errorMessage = "Identifiants incorrects!";
+          this.invalidCredentials = true;
+        } 
+        else {
+          this.errorMessage = "Une erreur s'est produite lors de la connexion!";
+        }
       }
     );
   }
