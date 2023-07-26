d1692b4b103f85bcc442af081e8606dfaec05ad4
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-07-26 12:46:23 +0200

    add salt to account activation token

--- api/src/Utility/TokenGenerator.php
+++ api/src/Utility/TokenGenerator.php
@@ -5,9 +5,10 @@ namespace App\Utility;
 class TokenGenerator
 
 {
-    public static function generateToken(): string
-    {
-        return bin2hex(random_bytes(32));
+    public static function generateToken(): string {
+        $salt = '$ATK$';
+        $token = bin2hex(random_bytes(32));
+        return $salt . $token;
     }
     
 }
