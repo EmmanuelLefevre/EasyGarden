e3f1898542005d9b0b724dcff4937ff94280bece
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-07-27 19:50:49 +0200

    refact

--- api/src/Utility/DateTimeConverter.php
+++ api/src/Utility/DateTimeConverter.php
@@ -8,24 +8,26 @@ class DateTimeConverter
 {
     public static function convertDateTimeStringToDateTimeImmutable(): \DateTimeImmutable
     {
-        // Get the user TimeZone and DateTimeZone array from class UserTimeZoneAndDateTimeDetector
-        $userData = self::extractUserTimeZoneAndDateTimeZone();
+        // Get the user TimeZone and DateTimeZone array from extractUserTimeZoneAndDateTimeZone method
+        $userData = self::extractUserData();
 
+        // Set the default timezone to the user's timezone
+        self::setDefaultTimeZone($userData['userTimeZone']);
         // Convert the date_time string to DateTimeImmutable object using the user's time zone
         $dateTimeImmutable = \DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $userData['userDateTimeZone']);
-        $dateTimeImmutable = $dateTimeImmutable->setTimezone(new \DateTimeZone($userData['userTimeZone']));
 
         return $dateTimeImmutable;
     }
 
     public static function convertDateTimeStringToDate(): \DateTime
     {
-        // Get the user TimeZone and DateTimeZone array from class UserTimeZoneAndDateTimeDetector
-        $userData = self::extractUserTimeZoneAndDateTimeZone();
+        // Get the user TimeZone and DateTimeZone array from extractUserData method
+        $userData = self::extractUserData();
 
+        // Set the default timezone to the user's timezone
+        self::setDefaultTimeZone($userData['userTimeZone']);
         // Convert the date_time string to DateTime object using the user's time zone
         $dateTime = \DateTime::createFromFormat('Y-m-d H:i:s', $userData['userDateTimeZone']);
-        $dateTime = $dateTime->setTimezone(new \DateTimeZone($userData['userTimeZone']));
 
         return $dateTime;
     }
@@ -36,11 +38,16 @@ class DateTimeConverter
         return UserTimeZoneAndDateTimeDetector::userTimeZoneAndDateTimeDetector();
     }
 
-    private static function extractUserTimeZoneAndDateTimeZone(): array
+    private static function extractUserData(): array
     {
         // Get user's TimeZone and DateTimeZone data from getUserData method
         $userData = self::getUserData();
 
         return $userData;
+    }
+
+    private static function setDefaultTimeZone(string $timeZone): void
+    {
+        date_default_timezone_set($timeZone);
     }
 }
