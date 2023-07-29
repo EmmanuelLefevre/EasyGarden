57eca7add77dd7c7f025cb58e7a685520f233a01
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-07-30 00:10:53 +0200

    add api route + comments

--- pwa/src/environments/environment.ts
+++ pwa/src/environments/environment.ts
@@ -4,38 +4,52 @@
 
 export const environment = {
   production: false,
+  // API
   apis: {
+    // Login
     login: {
       url: 'http://localhost:8000/api/login_check'
+    },
+    checkIfEmailExist: {
+      url: 'http://localhost:8000/api/check_if_email_exist'
     },
     checkAccountActivation: {
       url: 'http://localhost:8000/api/check_account_validation'
     },
+    // Register
     register: {
       url: 'http://localhost:8000/api/account_creation'
     },
+    // User
     user: {
       url: 'http://localhost:8000/api/users'
     },
+    // Garden
     garden: {
       url: 'http://localhost:8000/api/gardens'
     },
+    // Lawnmower
     lawnmower: {
       url: 'http://localhost:8000/api/lawnmowers'
     },
+    // Lightning
     lightning: {
       url: 'http://localhost:8000/api/lightnings'
     },
+    // Pool
     pool: {
       url: 'http://localhost:8000/api/pools'
     },
+    // Portal
     portal: {
       url: 'http://localhost:8000/api/portals'
     },
+    // Watering
     watering: {
       url: 'http://localhost:8000/api/waterings'
     }
   },
+  // Application
   application: {
     name: 'Easy Garden',
     colorTheme: '#95cb11',
