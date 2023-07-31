8d011f3a36fcc1b42aeb56a5bd8da2e8b9dbc5e6
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-07-31 03:01:28 +0200

    delete useless selector/component load in routing

--- pwa/src/app/public/components/home/home.component.ts
+++ pwa/src/app/public/components/home/home.component.ts
@@ -5,7 +5,6 @@ import { environment } from 'src/environments/environment';
 
 
 @Component({
-  selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   encapsulation: ViewEncapsulation.None
