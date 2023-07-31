f1023b93f60cfc357af69122afe7478ca5ee1524
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-01 00:08:18 +0200

    change message

--- api/src/EventSubsciber/LoginCheckSubscriber.php
+++ api/src/EventSubsciber/LoginCheckSubscriber.php
@@ -48,7 +48,7 @@ class LoginCheckSubscriber implements EventSubscriberInterface
             // Verify the presence of the required keys
             if (!isset($data['email']) || !isset($data['password'])) {
                 // The required keys are not present in the JSON, return an error response with 400 status
-                $response = new JsonResponse(['message' => 'Missing email or password!'], Response::HTTP_BAD_REQUEST);
+                $response = new JsonResponse(['message' => 'Invalid required keys!'], Response::HTTP_BAD_REQUEST);
                 $event->setResponse($response);
                 return;
             }
