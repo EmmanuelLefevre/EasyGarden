4f0f1089b7ebbae09d8d398fbaee034dd9c88a58
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-07-31 03:19:12 +0200

    add logic for isUserLogged()

--- pwa/src/app/_services/guard/auth.guard.ts
+++ pwa/src/app/_services/guard/auth.guard.ts
@@ -1,27 +1,34 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-
+import { map, Observable } from 'rxjs';
+// Service
+import { AuthService } from '../auth/auth.service';
 import { TokenService } from '../../_services/auth/token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class AuthGuard implements CanActivate {
+export class AuthGuardService implements CanActivate {
 
-  constructor(private router: Router,
+  constructor(private authService: AuthService,
+              private router: Router,
               private tokenService: TokenService) { }
 
   canActivate(
-    _route: ActivatedRouteSnapshot,
-    _state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    _route?: ActivatedRouteSnapshot,
+    _state?: RouterStateSnapshot): 
+      Observable<boolean | UrlTree> 
+      | Promise<boolean | UrlTree> 
+      | boolean 
+      | UrlTree {
 
-      if(this.tokenService.isLogged()){
-        return true
+      if (this.tokenService.isToken()) {
+        return this.authService.isUserLogged()
+          .pipe(map((isUserLogged) => isUserLogged));
       }
 
-    return this.router.navigate(['/'])
+    return this.router.navigate(['/']);
 
   }
   
