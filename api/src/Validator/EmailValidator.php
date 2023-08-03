681350a5b0b65a0e444ef12cc8d32f9181a2b293
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-03 12:51:19 +0200

    factorize methods in one

--- api/src/Validator/EmailValidator.php
+++ api/src/Validator/EmailValidator.php
@@ -7,24 +7,69 @@ use Symfony\Component\HttpFoundation\Request;
 
 class EmailValidator
 {
-   /**
+    /**
      * Check if the "email" param is defined in the request, is a string, and its value is not null or empty.
      * If any of these conditions are not met, return false; otherwise, return true.
-     * @param Request $request The HTTP request object.
      * @param string $paramName The parameter name for the email address to be checked.
+     * @param Request|null $request The HTTP request object or null if not provided.
      * @return bool True if the email is valid; otherwise, false.
      */
-    public function isValidEmailParam(Request $request, string $paramName): bool
+    // public function isValidEmail(string $paramName, ?Request $request = null): bool
+    // {
+    //     // If the $request is null, validate the $paramName directly
+    //     if ($request === null) {
+    //         // Check email max length, not null or empty and is a string
+    //         if (strlen($paramName) > 45 
+    //             || trim($paramName) === ''
+    //             || !is_string($paramName)) {
+    //             return false;
+    //         }
+
+    //         // Check if email is in a valid format
+    //         if (!filter_var($paramName, FILTER_VALIDATE_EMAIL)) {
+    //             return false;
+    //         }
+
+    //         return true;
+    //     }
+
+    //     // Check if the email parameter is present in the query parameters of the request
+    //     if (!$request->query->has($paramName)) {
+    //         return false;
+    //     }
+
+    //     $email = $request->query->get($paramName);
+
+    //     // Check max length of email, not null or empty and is a string
+    //     if (strlen($email) > 45 
+    //         || trim($email) === ''
+    //         || !is_string($paramName)) {
+    //         return false;
+    //     }
+
+    //     // Check if email is in a valid format
+    //     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    //         return false;
+    //     }
+
+    //     return true;
+    // }
+    public function isValidEmail(string $paramName, ?Request $request = null): bool
     {
-        // Check if the email parameter is present in the query parameters of the request
-        if (!$request->query->has($paramName)) {
-            return false;
+        // If the $request is null, validate the $paramName directly
+        if ($request === null) {
+            $email = $paramName;
+        } else {
+            // Check if the email parameter is present in the query parameters of the request
+            if (!$request->query->has($paramName)) {
+                return false;
+            }
+
+            $email = $request->query->get($paramName);
         }
 
-        $email = $request->query->get($paramName);
-
-        // Check max length of email and not null or empty
-        if (strlen($email) > 45 | trim($email) === '') {
+        // Check email max length, not null or empty and is a string
+        if (strlen($email) > 45 || trim($email) === '' || !is_string($email)) {
             return false;
         }
 
@@ -35,27 +80,4 @@ class EmailValidator
 
         return true;
     }
-
-    /**
-     * Check if the "email" param is defined in the request, is a string, and its value is not null or empty.
-     * If any of these conditions are not met, return false; otherwise, return true.
-     * @param Request $request The HTTP request object.
-     * @param string $paramName The parameter name for the email address to be checked.
-     * @return bool True if the email is valid; otherwise, false.
-     */
-    public function isValidEmail(string $paramName): bool
-    {
-        // Check max length of email and not null or empty
-        if (strlen($paramName) > 45 | trim($paramName) === '') {
-            return false;
-        }
-
-        // Check if email is in a valid format
-        if (!filter_var($paramName, FILTER_VALIDATE_EMAIL)) {
-            return false;
-        }
-
-        return true;
-    }
-
 }
