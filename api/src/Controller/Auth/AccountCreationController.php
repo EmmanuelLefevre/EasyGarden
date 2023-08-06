8a5002be0c9699d49d58adb3a3d4f13029a4924b
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-06 15:56:03 +0200

    use order

--- api/src/Controller/Auth/AccountCreationController.php
+++ api/src/Controller/Auth/AccountCreationController.php
@@ -12,8 +12,8 @@ use App\Utility\Token\TokenGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
