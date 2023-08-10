a752140e9d3cebf595ab906082c97ef5bceb6cc3
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-10 22:44:34 +0200

    add class annotation

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -5,8 +5,19 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+
+/**
+ * Class AppFixtures
+ * This class serves as a base for loading initial data fixtures into the database.
+ * It provides a starting point for creating and managing fixture data.
+ * @package App\DataFixtures
+ */
 class AppFixtures extends Fixture
 {
+    /**
+     * Load initial data fixtures into the database.
+     * @param ObjectManager $manager The object manager for interacting with the database.
+     */
     public function load(ObjectManager $manager): void
     {
         $manager->flush();
