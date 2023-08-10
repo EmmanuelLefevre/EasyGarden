3f90bfaa67a1ad0294708926696ba477c0702817
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-10 23:04:07 +0200

    add class annotation

--- api/src/Validator/User/EmailValidator.php
+++ api/src/Validator/User/EmailValidator.php
@@ -7,6 +7,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
+/**
+ * Validator to check the validity of an email address.
+ * @package App\Validator\User
+ */
 class EmailValidator
 {
     /**
