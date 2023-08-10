49799f99d749329ac02af3f653c7e96e95b24d3a
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-10 22:31:36 +0200

    add email validator logic in service

--- api/src/Service/Json/JsonDataValidatorService.php
+++ api/src/Service/Json/JsonDataValidatorService.php
@@ -3,21 +3,35 @@
 namespace App\Service\Json;
 
 use App\Utility\Json\JsonValidationException;
+use App\Validator\User\EmailValidator;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 
 /**
- * Service responsible for validating the json format of the request.
+ * Service responsible for validating the json format of the incoming request and performing additional validations.
+ * @package App\Service\Json
  */
 class JsonDataValidatorService
 {
+	private $emailValidator;
+
+	/**
+     * JsonDataValidatorService constructor.
+     * @param EmailValidator $emailValidator The service for validating email.
+     */
+    public function __construct(EmailValidator $emailValidator)
+    {
+        $this->emailValidator = $emailValidator;
+    }
+
 	/**
      * Validating json data from request.
      * @param Request $request The HTTP request object.
      * @param array $requiredKeys The required keys in the json data.
      * @return JsonResponse|array The validated JSON data or a JSON response in case of error.
-     */
+	 * @throws JsonValidationException If the JSON data is invalid or required keys are missing or empty.
+	 */
     public function validateJsonData(Request $request, array $requiredKeys): JsonResponse|array
     {
         $data = json_decode($request->getContent(), true);
@@ -32,6 +46,14 @@ class JsonDataValidatorService
             if (!isset($data[$field]) || empty($data[$field])) {
 				throw new JsonValidationException('Missing required key or empty field!');
 			}
+        }
+
+		// Validate email if present
+        if (isset($data['email'])) {
+            $isValidEmail = $this->emailValidator->isValidEmail($data['email'], true);
+            if ($isValidEmail instanceof JsonResponse) {
+                throw new JsonValidationException($isValidEmail->getContent());
+            }
         }
 
         return $data;
