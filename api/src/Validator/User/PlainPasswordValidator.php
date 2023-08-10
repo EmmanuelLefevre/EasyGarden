5e4f4fab9ec4e65854d1857a0c236b90e521a510
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-10 23:05:30 +0200

    add class annotation

--- api/src/Validator/User/PlainPasswordValidator.php
+++ api/src/Validator/User/PlainPasswordValidator.php
@@ -7,6 +7,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
+/**
+ * Validator to check the validity of a plain passsword.
+ * @package App\Validator\User
+ */
 class PlainPasswordValidator
 {
     /**
