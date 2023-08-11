95c664c13071c1908f8174ce988f68b99491ba29
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-11 22:37:38 +0200

    delete regex annotation

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -80,11 +80,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $pseudo;
 
     #[ORM\Column(type: 'string', length: 20)]
-    #[Assert\Regex(
-        pattern: '/\^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\$/',
-        match: true,
-        message: 'Please fill out a valid french number!',
-    )]
     #[Groups(['read:User',
               'write:User'])]
     private $phoneNumber;
