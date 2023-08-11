6f6904696d54dce29ab5f26ee001cf3619d37da2
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-11 14:35:24 +0200

    comments errors

--- api/src/Validator/User/NameValidator.php
+++ api/src/Validator/User/NameValidator.php
@@ -14,10 +14,10 @@ use Symfony\Component\HttpFoundation\Response;
 class NameValidator
 {
     /**
-     * Check if the "name" param is defined in the request, is a string, and its value is not null or empty.
+     * Check if the "firstName" or "lastName" param are defined in the request, are a string, and their value are not null or empty.
      * If any of these conditions are not met, return false; otherwise, return true.
      * @param Request|null $request The HTTP request object or null if not provided.
-     * @return bool|JsonResponse True if the name is valid; otherwise, false or JsonResponse with/no error message.
+     * @return bool|JsonResponse True if "firstName" or "lastName" are valid; otherwise, false or JsonResponse with/no error message.
      */
     public function isValidName(string $paramName,
                                 bool $returnJsonResponse = false,
@@ -27,7 +27,7 @@ class NameValidator
         if ($request === null) {
             $name = $paramName;
         } else {
-            // Check if the plain password parameter is present in the request
+            // Check if lastName and firstName parameter are present in the request
             if (!$request->request->has($paramName)) {
                 return $returnJsonResponse ? new JsonResponse('', Response::HTTP_BAD_REQUEST) : false;
             }
