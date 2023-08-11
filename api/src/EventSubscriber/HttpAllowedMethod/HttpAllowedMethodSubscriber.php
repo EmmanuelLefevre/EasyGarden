6ec65bfe5a282cb840708683ed8727d6bf7a2959
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-11 18:01:29 +0200

    subscriber custom jsonresponse not allowed method

---
+++ api/src/EventSubscriber/HttpAllowedMethod/HttpAllowedMethodSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\EventSubscriber\HttpAllowedMethod;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
+
+/**
+ * Class HttpAllowedMethodSubscriber
+ * This class subscribes to the kernel.exception event to perform custom JsonResponse.
+ * @package App\EventSubscriber\HttpAlloweMethod
+ */
+class HttpAllowedMethodSubscriber implements EventSubscriberInterface
+{
+    /**
+     * Handle the kernel exception
+     * @param ExceptionEvent $event The event when an not allowed method occurs.
+     */
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            $response = new JsonResponse(['message' => 'This endpoint only supports specified HTTP methods.'], JsonResponse::HTTP_METHOD_NOT_ALLOWED);
+            $event->setResponse($response);
+        }
+    }
+
+    /**
+     * Specifies the subscribed events and their priorities
+     * 
+     * @return array The array of subscribed events
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => ['onKernelException',EventPriorities::PRE_RESPOND]
+        ];
+    }
+
+}
