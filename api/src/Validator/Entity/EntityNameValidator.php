73de4d90bfad78265122f0d03f362d05c9493f9f
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-12 16:42:45 +0200

    indentation

--- api/src/Validator/Entity/EntityNameValidator.php
+++ api/src/Validator/Entity/EntityNameValidator.php
@@ -21,8 +21,8 @@ class EntityNameValidator
      * @return bool|JsonResponse True if the entityName is valid; otherwise, false or JsonResponse with/no error message.
      */
     public function isValidEntityName(string $paramName,
-                                bool $returnJsonResponse = false,
-                                ?Request $request = null): JsonResponse|bool
+                                      bool $returnJsonResponse = false,
+                                      ?Request $request = null): JsonResponse|bool
     {
         // If the $request is null, validate the $paramName directly
         if ($request === null) {
