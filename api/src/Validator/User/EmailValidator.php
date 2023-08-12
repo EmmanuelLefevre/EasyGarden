02a6e237e36d64984dcaef3f9feff13d1ec1a291
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-12 17:02:50 +0200

    add case to check parameter name

--- api/src/Validator/User/EmailValidator.php
+++ api/src/Validator/User/EmailValidator.php
@@ -38,6 +38,10 @@ class EmailValidator
         }
 
         switch (true) {
+            // Invalid email parameter name
+            case $email !== 'email':
+                return $returnJsonResponse ? new JsonResponse('', Response::HTTP_BAD_REQUEST) : false;
+
             // Not null
             case trim($email) === '':
                 return $returnJsonResponse ? new JsonResponse('', Response::HTTP_BAD_REQUEST) : false;
