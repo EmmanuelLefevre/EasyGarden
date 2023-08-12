121c19c9ff8d82cf186e966cf99b0e1b16421351
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-12 16:09:10 +0200

    validator for status equipment

---
+++ api/src/Validator/Entity/StatusValidator.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Validator\Entity;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+/**
+ * Validator to check the validity of status equipment.
+ * @package App\Validator\Entity
+ */
+class StatusValidator
+{
+    /**
+     * Check if the "status" param is defined in the request, is a boolean, and its value is not null or empty.
+     * If any of these conditions are not met, return false; otherwise, return true.
+     * @param string $paramName The name of the status parameter.
+     * @param Request|null $request The HTTP request object or null if not provided.
+     * @return bool|JsonResponse True if the status is valid; otherwise, false or JsonResponse with/no error message.
+     */
+    public function isValidStatus(string $paramName,
+                                  bool $returnJsonResponse = false,
+                                  ?Request $request = null): JsonResponse|bool
+    {
+        // If the $request is null, validate the $paramName directly
+        if ($request === null) {
+            $status = $paramName;
+        } else {
+            // Check if the status parameter is present in the request
+            if (!$request->request->has($paramName)) {
+                return $returnJsonResponse ? new JsonResponse('', Response::HTTP_BAD_REQUEST) : false;
+            }
+
+            $status = $request->request->get($paramName);
+        }
+
+        switch (true) {
+            // Not null
+            case trim($status) === '':
+                return $returnJsonResponse ? new JsonResponse(['error' => 'Null!'], Response::HTTP_BAD_REQUEST) : false;
+
+            // Is a boolean
+            case !is_bool($status):
+                return $returnJsonResponse ? new JsonResponse(['error' => 'Not boolean!'], Response::HTTP_BAD_REQUEST) : false;
+
+            // Check if the value is either true or false
+            if ($status !== true && $status !== false) {
+                return $returnJsonResponse ? new JsonResponse(['error' => 'not true, not false!'], Response::HTTP_BAD_REQUEST) : false;
+            }
+
+            default:
+                return true;
+        }
+    }
+
+}
