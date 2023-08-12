a6d043f0ed626f1e9fdda53f425c0de4f43bae7b
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-12 16:42:40 +0200

    indentation

--- api/src/Validator/Entity/BatterySensorValidator.php
+++ api/src/Validator/Entity/BatterySensorValidator.php
@@ -21,8 +21,8 @@ class BatterySensorValidator
      * @return bool|JsonResponse True if the batterySensor is valid; otherwise, false or JsonResponse with/no error message.
      */
     public function isValidBatterySensor(string $paramName,
-                                bool $returnJsonResponse = false,
-                                ?Request $request = null): JsonResponse|bool
+                                         bool $returnJsonResponse = false,
+                                         ?Request $request = null): JsonResponse|bool
     {
         // If the $request is null, validate the $paramName directly
         if ($request === null) {
