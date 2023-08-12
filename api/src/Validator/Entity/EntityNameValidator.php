027fc255d36afb45dfb05490499660939274f8bc
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-12 16:08:28 +0200

    add @param string in method annotation

--- api/src/Validator/Entity/EntityNameValidator.php
+++ api/src/Validator/Entity/EntityNameValidator.php
@@ -16,6 +16,7 @@ class EntityNameValidator
     /**
      * Check if the "entityName" param is defined in the request, is a string, and its value is not null or empty.
      * If any of these conditions are not met, return false; otherwise, return true.
+     * @param string $paramName The name of the entity name parameter.
      * @param Request|null $request The HTTP request object or null if not provided.
      * @return bool|JsonResponse True if the entityName is valid; otherwise, false or JsonResponse with/no error message.
      */
