32e3324d4455458f5486c3914b48e1bb9361376e
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-12 16:08:34 +0200

    add @param string in method annotation

--- api/src/Validator/User/NameValidator.php
+++ api/src/Validator/User/NameValidator.php
@@ -16,6 +16,7 @@ class NameValidator
     /**
      * Check if the "firstName" or "lastName" param are defined in the request, are a string, and their value are not null or empty.
      * If any of these conditions are not met, return false; otherwise, return true.
+     * @param string $paramName The name of the name parameter.
      * @param Request|null $request The HTTP request object or null if not provided.
      * @return bool|JsonResponse True if "firstName" or "lastName" are valid; otherwise, false or JsonResponse with/no error message.
      */
