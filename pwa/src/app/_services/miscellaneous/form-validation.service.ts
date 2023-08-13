ab318f66a2fbec8954664c19336ccffc321c4f4c
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-14 01:09:25 +0200

    change regex

--- pwa/src/app/_services/miscellaneous/form-validation.service.ts
+++ pwa/src/app/_services/miscellaneous/form-validation.service.ts
@@ -107,7 +107,7 @@ export class FormValidationService {
     return (control: AbstractControl): { [key: string]: boolean } | null => {
       if (control.value == '') return null;
 
-      let re = new RegExp('^([a-zA-Z- ]){3,25}$');
+      let re = new RegExp('^([a-zA-Z -]){3,25}$');
       if (re.test(control.value)) {
         return null;
       } else {
@@ -133,7 +133,7 @@ export class FormValidationService {
     return (control: AbstractControl): { [key: string]: boolean } | null => {
       if (control.value == '') return null;
 
-      let re = new RegExp('^[A-Za-z -.]{3,25}$');
+      let re = new RegExp('^([a-zA-Z -()]){3,25}$');
       if (re.test(control.value)) {
         return null;
       } else {
