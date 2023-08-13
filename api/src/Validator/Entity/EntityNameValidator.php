fcf79434ecea53c33e92a38c2c57202158b6b26e
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-14 01:10:24 +0200

    change regex

--- api/src/Validator/Entity/EntityNameValidator.php
+++ api/src/Validator/Entity/EntityNameValidator.php
@@ -56,8 +56,8 @@ class EntityNameValidator
                                                               Response::HTTP_BAD_REQUEST) : false;
             
             // Authorized character
-            case !preg_match('/[A-Za-z -.]/', $entityName):
-                return $returnJsonResponse ? new JsonResponse(['error' => 'Name can only contain lowercase or uppercase letters (dot, hyphens and spaces are accepted)!'], 
+            case !preg_match('/[A-Za-z -()]/', $entityName):
+                return $returnJsonResponse ? new JsonResponse(['error' => 'Name can only contain lowercase or uppercase letters (hyphens, parenthesis and spaces are accepted)!'], 
                                                               Response::HTTP_BAD_REQUEST) : false;
 
             default:
