013adcb1dbf65c5730aabcc1d2399fe8ca0373d2
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-13 16:59:12 +0200

    logic error + error message

--- api/src/Validator/User/PhoneNumberValidator.php
+++ api/src/Validator/User/PhoneNumberValidator.php
@@ -46,13 +46,13 @@ class PhoneNumberValidator
                 return $returnJsonResponse ? new JsonResponse('', Response::HTTP_BAD_REQUEST) : false;
 
             // Min length
-            case strlen($phoneNumber) < 10:
-                return $returnJsonResponse ? new JsonResponse(['error' => 'Phone number must contain at least 3 characters!'], 
+            case strlen($phoneNumber) < 9:
+                return $returnJsonResponse ? new JsonResponse(['error' => 'Phone number must contain at least 8 diggits!'], 
                                                               Response::HTTP_BAD_REQUEST) : false;
 
             // Max lenght
-            case strlen($phoneNumber) > 15:
-                return $returnJsonResponse ? new JsonResponse(['error' => 'Phone number cannnot exceed 25 characters!'], 
+            case strlen($phoneNumber) > 16:
+                return $returnJsonResponse ? new JsonResponse(['error' => 'Phone number cannnot exceed 15 characters!'], 
                                                               Response::HTTP_BAD_REQUEST) : false;
             
             // Authorized character
