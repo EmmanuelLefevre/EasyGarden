2f701bf83c2649e2df7ac183fe04d5fc4f4ed2ae
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-15 01:32:00 +0200

    change validation

--- pwa/src/app/_services/miscellaneous/form-validation.service.ts
+++ pwa/src/app/_services/miscellaneous/form-validation.service.ts
@@ -120,7 +120,7 @@ export class FormValidationService {
     return (control: AbstractControl): { [key: string]: boolean } | null => {
       if (control.value == '') return null;
 
-      let re = new RegExp('^([0-9a-zA-Z -]){3,20}$');
+      let re = new RegExp('^([0-9a-zA-Z -]){3,25}$');
       if (re.test(control.value)) {
         return null;
       } else {
