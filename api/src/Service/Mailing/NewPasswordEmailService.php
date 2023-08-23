abda364ab7d2188b514f4464e6e4917f0dbab5f9
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-23 14:40:59 +0200

    add service class for sending new pwd email

---
+++ api/src/Service/Mailing/NewPasswordEmailService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Service\Mailing;
+
+use App\Entity\User;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+
+/**
+ * Service to send new password emails to users.
+ * @package App\Service\Mailing
+ */
+class NewPasswordEmailService
+{
+
+}
