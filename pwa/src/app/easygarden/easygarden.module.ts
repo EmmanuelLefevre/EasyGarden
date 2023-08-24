b7a42350239741424b1997a6ebbb5c038a4836bb
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-24 13:57:47 +0200

    add comments

--- pwa/src/app/easygarden/easygarden.module.ts
+++ pwa/src/app/easygarden/easygarden.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DirectivesModule } from '../_directives/directives.module';
-
+// Routing
+import { EasygardenRoutingModule } from './easygarden-routing.module';
+// Modules
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ConfirmDialogModule } from './components/confirmDialog/confirm-dialog.module';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -10,13 +12,11 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { OrderModule } from 'ngx-order-pipe';
 import { FilterPipeModule } from 'ngx-filter-pipe';
 import { MainPipeModule } from './pipe/pipe.module';
-
-import { EasygardenRoutingModule } from './easygarden-routing.module';
-
-import { GardenComponent } from './components/garden/garden.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { EditEntityNameModule } from './components/editEntityName/edit-entity-name.module';
 import { AddEntityModule } from './components/addEntity/add-entity.module';
+// Components
+import { GardenComponent } from './components/garden/garden.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
