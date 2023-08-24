f5dd65a522008716f7e9f44193b0f51e66e08a2f
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-24 13:54:49 +0200

    inject logo module in public module

--- pwa/src/app/components/public-components.module.ts
+++ pwa/src/app/components/public-components.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+// Modules
 import { CloseFormButtonModule } from './closeFormButton/close-form-button.module';
+import { EasygardenLogoModule } from './easyGardenLogo/easygarden-logo.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    CloseFormButtonModule
+    CloseFormButtonModule,
+    EasygardenLogoModule
   ],
   exports: [
-    CloseFormButtonModule
+    CloseFormButtonModule,
+    EasygardenLogoModule
   ]
 })
 
