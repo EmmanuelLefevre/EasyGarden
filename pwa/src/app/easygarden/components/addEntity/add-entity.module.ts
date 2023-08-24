f2b30dad68f2e11c00a780a9f4724b8964a630b0
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-24 13:55:47 +0200

    add comments import

--- pwa/src/app/easygarden/components/addEntity/add-entity.module.ts
+++ pwa/src/app/easygarden/components/addEntity/add-entity.module.ts
@@ -2,10 +2,10 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { DirectivesModule } from 'src/app/_directives/directives.module';
-
+// Modules
 import { MatSelectModule } from '@angular/material/select';
 import { PublicComponentsModule } from 'src/app/components/public-components.module';
-
+// Components
 import { AddEntityComponent } from './add-entity.component';
 
 
