a09802a84657f2ba1d26a9c503ebed23debd5876
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-24 15:55:55 +0200

    add logic for expired token

--- pwa/src/app/_services/auth/auth.service.ts
+++ pwa/src/app/_services/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+// Services
+import { TokenService } from './token.service';
 // Modele
 import { ICredentials } from '../../_interfaces/ICredentials';
 import { IToken } from '../../_interfaces/IToken';
@@ -11,11 +13,18 @@ import { IToken } from '../../_interfaces/IToken';
   providedIn: 'root'
 })
 
+
 export class AuthService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+              private tokenService: TokenService) { }
 
   logIn(credentials: ICredentials): Observable<IToken>{
+    // Check if token is present and expired
+    const token = this.tokenService.getToken();
+    if (token !== null && this.tokenService.isTokenExpired(token)) {
+      this.tokenService.clearToken();
+    }
     return this.httpClient.post<IToken>(environment.apis.login.url, credentials);
   }
 
