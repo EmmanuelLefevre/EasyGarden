a9b3b77b43d1c704e626f8e8ea8b50c4ea1915da
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-24 13:59:04 +0200

    order import

--- pwa/src/app/public/public.module.ts
+++ pwa/src/app/public/public.module.ts
@@ -1,13 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// Routing
+import { PublicRoutingModule } from './public-routing.module';
 // Modules
 import { DirectivesModule } from '../_directives/directives.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { PublicComponentsModule } from '../components/public-components.module';
-// Routing
-import { PublicRoutingModule } from './public-routing.module';
 // Component
 import { ForgottenPasswordComponent } from './components/forgottenPassword/forgotten-password.component';
 import { HomeComponent } from './components/home/home.component';
