92146921027a20ebf8743e0dac3315cb9d82860e
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-24 14:04:59 +0200

    add comments for imports

--- pwa/src/app/easygarden/modules/portal/portal.module.ts
+++ pwa/src/app/easygarden/modules/portal/portal.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DirectivesModule } from 'src/app/_directives/directives.module';
-
+// Routing
+import { PortalRoutingModule } from './portal-routing.module';
+// Modules
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { OrderModule } from 'ngx-order-pipe';
 import { FilterPipeModule } from 'ngx-filter-pipe';
 import { MainPipeModule } from '../../pipe/pipe.module';
-
-import { PortalRoutingModule } from './portal-routing.module';
-
+// Components
 import { PortalComponent } from './portal.component';
 
 
