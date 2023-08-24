622c69688ebf2eed0adb912fb1cc0cea8f4135e9
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-08-24 14:29:19 +0200

    add scss file in ts file

--- pwa/src/app/components/easyGardenLogo/easygarden-logo.component.ts
+++ pwa/src/app/components/easyGardenLogo/easygarden-logo.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-easygarden-logo',
-  templateUrl: './easygarden-logo.component.html'
+  templateUrl: './easygarden-logo.component.html',
+  styleUrls: ['./easygarden-logo.component.scss']
 })
 
 
