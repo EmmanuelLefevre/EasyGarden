eada9d1d8b2c1df4d4d2815dab144f717a66c57c
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-08-24 13:55:19 +0200

    logo component ts

---
+++ pwa/src/app/components/easyGardenLogo/easygarden-logo.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-easygarden-logo',
+  templateUrl: './easygarden-logo.component.html'
+})
+
+
+export class EasygardenLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
