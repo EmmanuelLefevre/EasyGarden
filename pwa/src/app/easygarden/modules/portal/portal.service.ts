330c83add17341770f903672fbc7aa5cd3e51ae7
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-09-03 22:51:28 +0200

    modify service method for updateSatusController

--- pwa/src/app/easygarden/modules/portal/portal.service.ts
+++ pwa/src/app/easygarden/modules/portal/portal.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -39,7 +39,13 @@ export class PortalService {
 
   // Update Status
   updateStatus(status: boolean, id: number): Observable<IDataPortal[]> {
-    return this.httpClient.put<IDataPortal[]>(environment.apis.portal.url+'/'+id, {status});
+    // Create a custom HTTP headers object to specify the "lightning" type
+    const headers = new HttpHeaders({
+      'X-Type': 'portal'
+    });
+    // Use custom headers in HTTP request
+    const options = { headers: headers };
+    return this.httpClient.put<IDataPortal[]>(environment.apis.portal.url+'/'+id, {status}, options);
   }
 
   // Update Portal
