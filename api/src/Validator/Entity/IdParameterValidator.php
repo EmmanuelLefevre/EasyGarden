52d87c8f59f095ebafebb45efdcb68756ce87390
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-09-03 15:22:00 +0200

    new validator for check the id parameter

---
+++ api/src/Validator/Entity/IdParameterValidator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Validator\Entity;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+/**
+ * Validator to check the validity of id parameter.
+ * @package App\Validator\Entity
+ */
+class IdParameterValidator
+{
+    /**
+     * Check if parameter is a string and a number.
+     * @param string $paramValue The value of the parameter to validate.
+     * @param bool $returnJsonResponse Indicates whether an error JSON response should be returned if invalid.
+     * @return bool|JsonResponse True if the parameter is valid; otherwise, false or JsonResponse with/no error message.
+     */
+    public function isValidIdParameter(string $paramValue, bool $returnJsonResponse = false): JsonResponse|bool
+    {
+        switch (true) {
+            // Is a string
+            case !is_string($paramValue):
+                return $returnJsonResponse ? new JsonResponse('', Response::HTTP_BAD_REQUEST) : false;
+
+            // Is a number
+            case !is_numeric($paramValue):
+                return $returnJsonResponse ? new JsonResponse('', Response::HTTP_BAD_REQUEST) : false;
+
+            default:
+                return true;
+        }
+    }
+}
