2f8230059a19bf4071d9058d18b9803fd025caab
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-09-05 16:14:43 +0200

    renamed service

---
+++ api/src/Service/Header/XTypeValueService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Service\Header;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+
+/**
+ * Service for extracting the value of the 'X-Type' header from the current HTTP request.
+ * @package App\Service\Header
+ */
+class XTypeValueService
+{
+    private $requestStack;
+
+    /**
+     * XTypeHeaderValueService constructor.
+     * @param RequestStack $requestStack The RequestStack instance.
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * Get the value of the 'X-Type' header from the current HTTP request.
+     * @return string The value of the 'X-Type' header if it exists.
+     * @throws \Exception If the 'X-Type' header is missing, throw an exception.
+     */
+    public function getXTypeHeaderValue(): string
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        
+        if ($request) {
+            $xTypeHeader = $request->headers->get('X-Type');
+            if ($xTypeHeader !== null) {
+                return $xTypeHeader;
+            }
+        }
+
+        throw new \Exception('X-Type header is missing or invalid!');
+    }
+}
