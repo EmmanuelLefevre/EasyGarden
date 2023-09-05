ffb49a9c70e8ced760fe70dcf43d35bee7cd3ebf
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-09-05 22:41:17 +0200

    prepare service for arduino

--- api/src/Service/Arduino/ArduinoConnectionService.php
+++ api/src/Service/Arduino/ArduinoConnectionService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Service;
+namespace App\Service\Arduino;
 
 // Serial
 use PhpSerial\Serial;
@@ -11,68 +11,68 @@ use phpseclib\Net\BluetoothException;
 
 class ArduinoConnectionService
 {
-    private $bluetoothDevice;
-    private $serialDevice;
+    // private $bluetoothDevice;
+    // private $serialDevice;
 
-    public function __construct(string $bluetoothDevice,
-                                string $serialDevice)
-    {
-        $this->bluetoothDevice = $bluetoothDevice;
-        $this->serialDevice = $serialDevice;
-    }
+    // public function __construct(string $bluetoothDevice,
+    //                             string $serialDevice)
+    // {
+    //     $this->bluetoothDevice = $bluetoothDevice;
+    //     $this->serialDevice = $serialDevice;
+    // }
 
-    public function openSerialConnection(bool $data): void
-    {
-        $serial = new PhpSerial();
-        $serial->deviceSet($this->serialDevice);
-        $serial->confBaudRate(9600);
+    // public function openSerialConnection(bool $status): void
+    // {
+    //     $serial = new PhpSerial();
+    //     $serial->deviceSet($this->serialDevice);
+    //     $serial->confBaudRate(9600);
         
-        // Open connection and manage serial connection errors
-        if (!$serial->deviceOpen()) {
-            throw new \Exception('Impossible d\'ouvrir la connexion!');
-        }
+    //     // Open connection and manage serial connection errors
+    //     if (!$serial->deviceOpen()) {
+    //         throw new \Exception('Impossible d\'ouvrir la connexion!');
+    //     }
 
-        // Send boolean to Arduino
-        $success = $serial->sendMessage($data ? '1' : '0');
-        // $success = $serial->send($connection, $data ? 'true' : 'false');
+    //     // Send boolean to Arduino
+    //     $success = $serial->sendMessage($status ? '1' : '0');
+    //     // $success = $serial->sendMessage($connection, $status ? 'true' : 'false');
 
-        // Manage sending data errors
-        if (!$success) {
-            throw new \Exception('Échec de l\'envoi des données!');
-        }
+    //     // Manage sending status errors
+    //     if (!$success) {
+    //         throw new \Exception('Échec de l\'envoi des données!');
+    //     }
 
-        // Close connection
-        $serial->deviceClose();
-    }
+    //     // Close connection
+    //     $serial->deviceClose();
+    // }
 
-    public function openBluetoothConnection(bool $data): void
-    {
-        try {
-            $bluetooth = new Bluetooth();
-            $connection = $bluetooth->connect($this->bluetoothDevice);
+    // public function openBluetoothConnection(bool $status): void
+    // {
+    //     try {
+    //         $bluetooth = new Bluetooth();
+    //         $connection = $bluetooth->connect($this->bluetoothDevice);
 
-            if (!$connection) {
-                throw new \Exception('Impossible de se connecter à l\'Arduino!');
-            }
+    //         if (!$connection) {
+    //             throw new \Exception('Impossible de se connecter à l\'Arduino!');
+    //         }
 
-            // Send boolean to Arduino
-            $success = $bluetooth->send($connection, $data ? '1' : '0');
-            // $success = $bluetooth->send($connection, $data ? 'true' : 'false');
-            // Manage sending data errors
-            if (!$success) {
-                throw new \Exception('Échec de l\'envoi des données!');
-            }
+    //         // Send boolean to Arduino
+    //         $success = $bluetooth->send($connection, $status ? '1' : '0');
+    //         // $success = $bluetooth->send($connection, $status ? 'true' : 'false');
+    //         // Manage sending status errors
+    //         if (!$success) {
+    //             throw new \Exception('Échec de l\'envoi des données!');
+    //         }
 
-            // Close connection
-            $bluetooth->disconnect($connection);
+    //         // Close connection
+    //         $bluetooth->disconnect($connection);
 
-        } catch (BluetoothException $e) {
-            // Handle bluetooth library errors
-            throw new \Exception('Erreur Bluetooth : ' . $e->getMessage());
-        } catch (\Exception $e) {
-            // Handle other possible errors
-            throw $e;
-        }
-    }
+    //     } catch (BluetoothException $e) {
+    //         // Handle bluetooth library errors
+    //         throw new \Exception('Erreur Bluetooth : ' . $e->getMessage());
+    //     } catch (\Exception $e) {
+    //         // Handle other possible errors
+    //         throw $e;
+    //     }
+    // }
     
 }
