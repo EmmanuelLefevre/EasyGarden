482b608fdb854e4384ca4cb96422a4356a1fa5f6
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-09-11 18:03:58 +0200

    call get method

--- pwa/src/app/easygarden/modules/lightning/lightning.component.ts
+++ pwa/src/app/easygarden/modules/lightning/lightning.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 // Add ViewEncapsulation for resolve problems with loading custom scss .mat-tooltip-social in style.scss
+import { HttpResponse } from '@angular/common/http';
 import { Subject, Subscription, takeUntil } from 'rxjs';
 // Environment
 import { environment } from 'src/environments/environment';
@@ -164,9 +165,21 @@ export class LightningComponent implements OnInit, OnDestroy {
         this.result = dialogResult;
         if (this.result === true) {
           this.deleteLightningSubscription = this.lightningService.deleteLightning(id)
-            .subscribe(() => {
-              this.fetchLightnings();
-            });
+          .subscribe(
+            (response: HttpResponse<any>) => {
+              if (response.status === 204) {
+                const notificationMessage = this.snackbarService.getNotificationMessage();
+                this.snackbarService.showNotification(notificationMessage, 'deleted');
+                this.fetchLightnings();
+              }
+            },
+            (_error) => {
+              this.snackbarService.showNotification(
+                `Une erreur s'est produite!`,
+                'red-alert'
+              );
+            }
+          )
         }
       });
   }
