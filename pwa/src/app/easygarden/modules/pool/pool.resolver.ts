0249e398d33f2bc8a53a5ab1ed28799f68bc80ee
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-11-04 14:56:03 +0100

    forkjoin gardens/classes data

--- pwa/src/app/easygarden/modules/pool/pool.resolver.ts
+++ pwa/src/app/easygarden/modules/pool/pool.resolver.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Router, Resolve, ActivatedRouteSnapshot } from '@angular/router';
-import { catchError, Observable, throwError, delay } from 'rxjs';
-
+import { Router, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+// RXJS
+import { catchError, forkJoin, Observable, throwError } from 'rxjs';
+// Service
+import { GardenService } from '../../components/garden/garden.service';
 import { PoolService } from './pool.service';
+// Models
+import { IDataGarden } from '../../components/garden/IGarden';
 import { IDataPool } from './IPool';
 
 
@@ -10,21 +14,21 @@ import { IDataPool } from './IPool';
   providedIn: 'root'
 })
 
-export class PoolResolver implements Resolve<IDataPool[]> {
+export class PoolResolver implements Resolve<{ pools: IDataPool[], gardens: IDataGarden[] }> {
 
-  constructor(private poolService: PoolService,
+  constructor(private gardenService: GardenService,
+              private poolService: PoolService,
               private router: Router) {}
 
-  resolve(_route: ActivatedRouteSnapshot): Observable<IDataPool[]> {
-    
-    return this.poolService.getAllPools().pipe(
-      // delay(1000),
-      catchError(
-        () => {
-          this.router.navigate([""]);
-          return throwError(() => ('No pools were found.'))
-        }
-      )
+  resolve(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot): Observable<{ pools: IDataPool[], gardens: IDataGarden[] }> {
+    const pools$: Observable<IDataPool[]> = this.poolService.getAllPools();
+    const gardens$: Observable<IDataGarden[]> = this.gardenService.getAllGardens();
+
+    return forkJoin({ pools: pools$, gardens: gardens$ }).pipe(
+      catchError(() => {
+        this.router.navigate([""]);
+        return throwError(() => ('Failed to fetch data.'));
+      })
     );
   }
 
