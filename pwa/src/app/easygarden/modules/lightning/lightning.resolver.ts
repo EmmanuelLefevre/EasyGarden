5e4f9b6afdcbf3a97e18365da678ab0e20b3f2b2
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-11-04 14:55:55 +0100

    forkjoin gardens/classes data

--- pwa/src/app/easygarden/modules/lightning/lightning.resolver.ts
+++ pwa/src/app/easygarden/modules/lightning/lightning.resolver.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Router, Resolve, ActivatedRouteSnapshot } from '@angular/router';
-import { catchError, Observable, throwError, delay } from 'rxjs';
-
+import { Router, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+// RXJS
+import { catchError, forkJoin, Observable, throwError } from 'rxjs';
+// Service
+import { GardenService } from '../../components/garden/garden.service';
 import { LightningService } from './lightning.service';
+// Models
+import { IDataGarden } from '../../components/garden/IGarden';
 import { IDataLightning } from './ILightning';
 
 
@@ -10,21 +14,22 @@ import { IDataLightning } from './ILightning';
   providedIn: 'root'
 })
 
-export class LightningResolver implements Resolve<IDataLightning[]> {
-  
-  constructor(private lightningService: LightningService,
+export class LightningResolver implements Resolve<{ lightnings: IDataLightning[], gardens: IDataGarden[] }> {
+
+  constructor(private gardenService: GardenService,
+              private lightningService: LightningService,
               private router: Router) {}
 
-  resolve(_route: ActivatedRouteSnapshot): Observable<IDataLightning[]> {
-    return this.lightningService.getAllLightnings().pipe(
-      // delay(1000),
-      catchError(
-        () => {
-          this.router.navigate([""]);
-          return throwError(() => ('No lightnings were found.'))
-        }
-      )
+  resolve(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot): Observable<{ lightnings: IDataLightning[], gardens: IDataGarden[] }> {
+    const lightnings$: Observable<IDataLightning[]> = this.lightningService.getAllLightnings();
+    const gardens$: Observable<IDataGarden[]> = this.gardenService.getAllGardens();
+
+    return forkJoin({ lightnings: lightnings$, gardens: gardens$ }).pipe(
+      catchError(() => {
+        this.router.navigate([""]);
+        return throwError(() => ('Failed to fetch data.'));
+      })
     );
   }
-  
+
 }
