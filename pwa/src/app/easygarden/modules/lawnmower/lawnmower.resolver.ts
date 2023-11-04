cc24c766e895ac3cca283af2f3567e7251591b76
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-11-04 14:41:30 +0100

    forkjoin gardens/classes data

--- pwa/src/app/easygarden/modules/lawnmower/lawnmower.resolver.ts
+++ pwa/src/app/easygarden/modules/lawnmower/lawnmower.resolver.ts
@@ -1,29 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Router, Resolve, ActivatedRouteSnapshot } from '@angular/router';
-import { catchError, Observable, throwError, delay } from 'rxjs';
-
+import { Router, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+// RXJS
+import { catchError, Observable, throwError, forkJoin } from 'rxjs';
+// Service
+import { GardenService } from '../../components/garden/garden.service';
 import { LawnmowerService } from './lawnmower.service';
+// Models
 import { IDataLawnmower } from './ILawnmower';
+import { IDataGarden } from '../../components/garden/IGarden';
 
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class LawnmowerResolver implements Resolve<IDataLawnmower[]> {
+export class LawnmowerResolver implements Resolve<{ lawnmowers: IDataLawnmower[], gardens: IDataGarden[] }> {
 
-  constructor(private lawnmowerService: LawnmowerService,
+  constructor(private gardenService: GardenService,
+              private lawnmowerService: LawnmowerService,
               private router: Router) {}
 
-  resolve(_route: ActivatedRouteSnapshot): Observable<IDataLawnmower[]> {
-    return this.lawnmowerService.getAllLawnmowers().pipe(
-      // delay(1000),
-      catchError(
-        () => {
-          this.router.navigate([""]);
-          return throwError(() => ('No lawnmowers were found.'))
-        }
-      )
+  resolve(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot): Observable<{ lawnmowers: IDataLawnmower[], gardens: IDataGarden[] }> {
+    const lawnmowers$: Observable<IDataLawnmower[]> = this.lawnmowerService.getAllLawnmowers();
+    const gardens$: Observable<IDataGarden[]> = this.gardenService.getAllGardens();
+
+    return forkJoin({ lawnmowers: lawnmowers$, gardens: gardens$ }).pipe(
+      catchError(() => {
+        this.router.navigate([""]);
+        return throwError(() => ('Failed to fetch data.'));
+      })
     );
   }
 
