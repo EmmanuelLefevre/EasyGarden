f70bbfdd9fa646ba00c42641ff309fed09c09585
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-11-04 23:50:50 +0100

    maj service behavior subject

--- pwa/src/app/easygarden/modules/watering/watering.service.ts
+++ pwa/src/app/easygarden/modules/watering/watering.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { share } from 'rxjs/operators';
+// RXJS
+import { BehaviorSubject, Observable, share, tap } from 'rxjs';
 // Environment
 import { environment } from 'src/environments/environment';
 // Modeles
@@ -15,9 +15,15 @@ import { IAdd } from '../../_interfaces/IAdd';
 })
 
 export class WateringService {
-  
+  // Update status behavior subject
+  private updateStatusSubject = new BehaviorSubject<IDataWatering[]>([]);
+  public updateStatus$ = this.updateStatusSubject.asObservable();
+  // Delete watering behavior subject
+  private deleteWateringSubject = new BehaviorSubject<IDataWatering[]>([]);
+  public deleteWatering$ = this.deleteWateringSubject.asObservable();
+
   constructor(private httpClient: HttpClient) { }
-  
+
   // Get List of Waterings
   getAllWaterings(): Observable<IDataWatering[]> {
     return this.httpClient.get<IDataWatering[]>(environment.apis.watering.url).pipe(share());
@@ -48,7 +54,13 @@ export class WateringService {
     });
     // Use custom headers in HTTP request
     const options = { headers: headers };
-    return this.httpClient.put<IDataWatering[]>(environment.apis.watering.url+'/'+id, {status}, options);
+    return this.httpClient.put<IDataWatering[]>(environment.apis.watering.url+'/'+id, {status}, options)
+    .pipe(
+      tap((updatedStatusWaterings$) => {
+        // Update data locally
+        this.updateStatusSubject.next(updatedStatusWaterings$);
+      })
+    );
   }
 
   // Update Watering
@@ -58,12 +70,18 @@ export class WateringService {
 
   // Delete Watering
   deleteWatering(id: number): Observable<HttpResponse<any>> {
-    return this.httpClient.delete(environment.apis.watering.url+'/'+id, { observe: 'response' });
+    return this.httpClient.delete(environment.apis.watering.url+'/'+id, { observe: 'response' })
+    .pipe(
+      tap(() => {
+        const deletedWaterings = this.deleteWateringSubject.getValue().filter(watering => watering.id !== id);
+        this.deleteWateringSubject.next(deletedWaterings);
+      })
+    );
   }
 
   // Get redirection
   getRedirectUrl(): string {
     return '/easygarden/watering';
   }
-  
+
 }
