d800003995fa1867258ea1ea61dcddde95f0d47e
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2023-11-04 23:50:18 +0100

    maj service behavior subject

--- pwa/src/app/easygarden/modules/lightning/lightning.service.ts
+++ pwa/src/app/easygarden/modules/lightning/lightning.service.ts
@@ -4,8 +4,6 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, share, tap } from 'rxjs';
 // Environment
 import { environment } from 'src/environments/environment';
-// Services
-import { SnackbarService } from 'src/app/_services/miscellaneous/snackbar.service';
 // Modeles
 import { ILightning, IDataLightning } from './ILightning';
 import { IName } from '../../_interfaces/IName';
@@ -17,11 +15,14 @@ import { IAdd } from '../../_interfaces/IAdd';
 })
 
 export class LightningService {
-  private lightningsSubject = new BehaviorSubject<IDataLightning[]>([]);
-  public lightnings$ = this.lightningsSubject.asObservable();
+  // Update status behavior subject
+  private updateStatusSubject = new BehaviorSubject<IDataLightning[]>([]);
+  public updateStatus$ = this.updateStatusSubject.asObservable();
+  // Delete lightning behavior subject
+  private deleteLightningSubject = new BehaviorSubject<IDataLightning[]>([]);
+  public deleteLightning$ = this.deleteLightningSubject.asObservable();
 
-  constructor(private httpClient: HttpClient,
-              private snackbarService: SnackbarService) { }
+  constructor(private httpClient: HttpClient) { }
 
   // Get List of Lightnings
   getAllLightnings(): Observable<IDataLightning[]> {
@@ -53,9 +54,9 @@ export class LightningService {
     const options = { headers: headers };
     return this.httpClient.put<IDataLightning[]>(environment.apis.status.url+'/'+id, {status}, options)
     .pipe(
-      tap((updatedLightnings$) => {
+      tap((updatedStatusLightnings$) => {
         // Update data locally
-        this.lightningsSubject.next(updatedLightnings$);
+        this.updateStatusSubject.next(updatedStatusLightnings$);
       })
     );
   }
@@ -68,26 +69,12 @@ export class LightningService {
   // Delete Lightning
   deleteLightning(id: number): Observable<HttpResponse<any>> {
     return this.httpClient.delete(environment.apis.lightning.url + '/' + id, { observe: 'response' })
-      .pipe(
-        tap((res$) => {
-          if (res$.status === 204) {
-            this.refreshData();
-            const notificationMessage = this.snackbarService.getNotificationMessage();
-            this.snackbarService.showNotification(notificationMessage, 'deleted');
-          } else {
-            this.snackbarService.showNotification(
-              `Une erreur s'est produite lors de la suppression!`,
-              'red-alert'
-            );
-          }
-        })
-      );
-  }
-  refreshData() {
-    this.getAllLightnings()
-      .subscribe((deletedLightnings$) => {
-        this.lightningsSubject.next(deletedLightnings$);
-      });
+    .pipe(
+      tap(() => {
+        const deletedLightnings = this.deleteLightningSubject.getValue().filter(lightning => lightning.id !== id);
+        this.deleteLightningSubject.next(deletedLightnings);
+      })
+    );
   }
 
   // Get redirection
