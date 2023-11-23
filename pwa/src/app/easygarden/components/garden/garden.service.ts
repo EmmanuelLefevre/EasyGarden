ea2b79b2a69f71685fd80736950dec4e9358215b
Emmanuel Lefevre <waiasiaa@protonmail.com>
2023-11-23 12:51:37 +0100

    delete useless method

--- pwa/src/app/easygarden/components/garden/garden.service.ts
+++ pwa/src/app/easygarden/components/garden/garden.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
 // Services
 import { DecodedTokenService } from 'src/app/_services/miscellaneous/decoded-token.service';
 // Modeles
-import { IGarden, IDataGarden } from './IGarden';
+import { IDataGarden } from './IGarden';
 import { IName } from '../../_interfaces/IName';
 import { IAdd } from '../../_interfaces/IAdd';
 
@@ -35,11 +35,6 @@ export class GardenService {
       user: 'api/users/'+id
     };
     return this.httpClient.post(environment.apis.garden.url, json);
-  }
-
-  // Get Garden
-  getData(id: number | null): Observable<IGarden>{
-    return this.httpClient.get<IGarden>(environment.apis.garden.url+'/'+id);
   }
 
   // Update Garden
