30bdedb2371690825bf54f626b178b5cb8adb354
Emmanuel Lefevre <waiasiaa@protonmail.com>
2024-11-11 16:52:54 +0100

    maj

--- api/src/Service/Arduino/ArduinoConnectionService.php
+++ api/src/Service/Arduino/ArduinoConnectionService.php
@@ -17,7 +17,7 @@ class ArduinoConnectionService
      * Opens a serial connection and sends a command to the Arduino.
      *
      * @param string $status The status to send ("1" or "0").
-     * @return string Message indicating the result of the operation.
+     * @return string Message  Message from the Arduino.
      * @throws \Exception
      */
     public function openSerialConnection($status)
@@ -30,23 +30,49 @@ class ArduinoConnectionService
         // Configure serial port
         $command = "mode {$this->port}: baud={$this->baud} parity=N data=8 stop=1";
         exec($command, $output, $statusCode);
-
         if ($statusCode !== 0) {
             throw new \Exception("Unable to configure serial port on `$this->port`!");
         }
 
         // Open serial connection
         $serial = fopen($this->port, "w+");
-
         if (!$serial) {
             throw new \Exception("Unable to open serial port on `$this->port`!");
         }
 
-        fwrite($serial, $status);
+        try {
+            // Send command to Arduino
+            fwrite($serial, $status);
 
-        // Close serial connection
-        fclose($serial);
+            // Waiting for the response from the Arduino with a controlled loop
+            $attempt = 0;
+            $maxAttempts = 10;
+            $arduinoResponse = "";
 
-        return "Serial connection has been successfully established on `$this->port` (command has been sent).";
+            while ($attempt < $maxAttempts) {
+                $arduinoResponse = fgets($serial);
+
+                // If a response is received, we exit the loop
+                if ($arduinoResponse !== false) {
+                    $arduinoResponse = str_replace(["\r", "\n"], '', $arduinoResponse);
+                    break;
+                }
+
+                $attempt++;
+            }
+
+            // Verify if response was actually received
+            if (empty($arduinoResponse || $arduinoResponse === false)) {
+                throw new \Exception("No response (or empty one) received from Arduino!");
+            }
+
+            $connectionMessage = "Serial connection has been successfully established on `$this->port`. " . PHP_EOL;
+            return $connectionMessage . "Arduino response: " . $arduinoResponse;
+        }
+        // Finally block => ensures that fclose($serial) is always called, even in the event of an exception.
+        finally {
+            // Close serial connection
+            fclose($serial);
+        }
     }
 }
