4fb9de404f3086b74a477e0975c9240e25c9f61b
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-11-12 18:10:15 +0100

    maj

--- sketch_Serial LED Arduino.ino
+++ sketch_Serial LED Arduino.ino
@@ -1,4 +1,5 @@
 const int ledPin = 5; // Pin to which the LED is connected
+String received = ""; // Variable to store received value
 int timeDelay = 20;
 
 void setup()
@@ -11,20 +12,24 @@ void loop() {
   // Check if data is available in the serial buffer
   if (Serial.available() > 0) {
     // Read incoming message from serial
-    string received = Serial.readStringUntil('\n');
+    received = Serial.readStringUntil('\n');
 
-    // If message is '1', turn on the LED
-    if (received == '1') {
+    // If the message is "1", turn on the LED
+    if (received == "1") {
       digitalWrite(ledPin, HIGH); // Turn on LED by setting pin HIGH
       delay(timeDelay); // Apply time delay
-      Serial.println("Eclairage allumé!");
+      sendMessage("Eclairage allumé!");
     }
 
-    // If message is '0', turn off the LED
-    if (received == '0') {
+    // If the message is "0", turn off the LED
+    if (received == "0") {
       digitalWrite(ledPin, LOW); // Turn off LED by setting pin LOW
       delay(timeDelay); // Apply time delay
-      Serial.println("Eclairage éteint!");
+      sendMessage("Eclairage éteint!");
     }
   }
+}
+
+void sendMessage(String message) {
+  Serial.println(message); // Send the message to the serial monitor
 }
