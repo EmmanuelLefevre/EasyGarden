4e0087565bd7773e2676a733c3a25518890647cf
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-11-14 08:17:12 +0100

    maj

--- api/src/Validator/Entity/PresenceSensorValidator.php
+++ api/src/Validator/Entity/PresenceSensorValidator.php
@@ -21,8 +21,8 @@ class PresenceSensorValidator
      * @return bool|JsonResponse True if the presenceSensor is valid; otherwise, false or JsonResponse with/no error message.
      */
     public function isValidPresenceSensor(string $paramName,
-                                  bool $returnJsonResponse = false,
-                                  ?Request $request = null): JsonResponse|bool
+                                          bool $returnJsonResponse = false,
+                                          ?Request $request = null): JsonResponse|bool
     {
         // If the $request is null, validate the $paramName directly
         if ($request === null) {
